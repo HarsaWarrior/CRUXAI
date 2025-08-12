829aed351759fe1810385ecc72e92cf1e10aa737
Harsh Patil <harsh3152002@gmail.com>
2025-08-12 14:08:17 +0530

    Created the Hero Section of the landing page and gave some styling to it

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import BgGradient from "@/components/common/bg-gradient";
+import HeroSection from "@/components/home/hero-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <Button variant="outline" size={'lg'}>Hello</Button>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+      {/* <DemoSection /> */}
+      {/* <HowItWorksSection /> */}
+      {/* <PricingSection /> */}
+      {/* <CTASection /> */}
     </div>
   );
 }
