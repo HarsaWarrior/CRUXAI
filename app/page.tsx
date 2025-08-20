d1f629aefb067bff02f3e9137d7ad39c11204db5
Harsh Patil <harsh3152002@gmail.com>
2025-08-20 19:17:41 +0530

    Completed the design for the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import BgGradient from "@/components/common/bg-gradient";
+import CTASection from "@/components/home/cta-section";
+import DemoSection from "@/components/home/demo-section";
 import HeroSection from "@/components/home/hero-section";
+import HowItWorksSection from "@/components/home/how-it-works";
+import PricingSection from "@/components/home/pricing-section";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
@@ -9,11 +13,11 @@ export default function Home() {
       <BgGradient />
       <div className="flex flex-col">
         <HeroSection />
+        <DemoSection />
+        <HowItWorksSection />
+        <PricingSection />
+        <CTASection />
       </div>
-      {/* <DemoSection /> */}
-      {/* <HowItWorksSection /> */}
-      {/* <PricingSection /> */}
-      {/* <CTASection /> */}
     </div>
   );
 }
